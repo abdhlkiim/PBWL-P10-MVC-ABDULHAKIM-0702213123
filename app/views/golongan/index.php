b63db122db9cb39ff7f1bcda773798326d5c5651
abdhlkiim <150272186+abdhlkiim@users.noreply.github.com>
2023-11-28 00:18:30 +0700

    update #no

--- app/views/golongan/index.php
+++ app/views/golongan/index.php
@@ -3,28 +3,28 @@
 <a href="<?php echo URL; ?>/golongan/input" class="btn">Input Golongan</a>
 
 <table id="dtb">
-    <thead>
-    <tr>
-            <th>NO</th>
-            <th>KODE</th>
-            <th>NAMA</th>
-            <th>EDIT</th>
-            <th>DELETE</th>
-      </tr>
-    </thead>
-    <tbody>
-        
-      <?php $no = 1;
-      foreach ($data['rows'] as $row) { ?>
+      <thead>
             <tr>
-                  <td><?php echo $no; ?></td>
-                  <td><?php echo $row['gol_kode']; ?></td>
-                  <td><?php echo $row['gol_nama']; ?></td>
-                  <td><a href="<?php echo URL; ?>/golongan/edit/<?php echo $row['gol_id']; ?>" class="btn">Edit</a></td>
-                  <td><a href="<?php echo URL; ?>/golongan/delete/<?php echo $row['gol_id']; ?>" class="btn" onclick="return confirm('Are you sure?')">Delete</a></td>
+                  <th>#NO</th>
+                  <th>KODE</th>
+                  <th>NAMA</th>
+                  <th>EDIT</th>
+                  <th>DELETE</th>
             </tr>
-      <?php $no++;
-      } ?>
+      </thead>
+      <tbody>
 
-    </tbody>
+            <?php $no = 1;
+            foreach ($data['rows'] as $row) { ?>
+                  <tr>
+                        <td><?php echo $no; ?></td>
+                        <td><?php echo $row['gol_kode']; ?></td>
+                        <td><?php echo $row['gol_nama']; ?></td>
+                        <td><a href="<?php echo URL; ?>/golongan/edit/<?php echo $row['gol_id']; ?>" class="btn">Edit</a></td>
+                        <td><a href="<?php echo URL; ?>/golongan/delete/<?php echo $row['gol_id']; ?>" class="btn" onclick="return confirm('Are you sure?')">Delete</a></td>
+                  </tr>
+            <?php $no++;
+            } ?>
+
+      </tbody>
 </table>
